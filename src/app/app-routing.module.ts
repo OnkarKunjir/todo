f15c8857bea2aa848fb49cf70679619868a2970b
Onkar Kunjir <onkarkunjir8@gmail.com>
2020-06-07 22:26:40 +0530

    base application. Added basic navbar and using nord color scheme.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { TodoComponent } from './todo/todo.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , component: LandingPageComponent},
+  {path: 'account' , component: TodoComponent}, // chage to todo 
+  {path :'sign-up' , component: SignUpComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LandingPageComponent,
+    SignUpComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
